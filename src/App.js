ad17a3a01211ed9e2c4c75bbf92b3be5004780d2
gospeller986 <palsatyajit986@gmail.com>
2022-02-24 00:19:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Time } from './Time';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles ((theme)=>({ 
+
+  root : {
+    
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    justifyContent : "center" ,
+    justifyItems :"center" ,
+    display : "flex" ,
+    maxHeight : "100vh",
+    marginTop : "20vh",
+    
+  }, 
+
+  container : {
+    backgroundImage: `linear-gradient(to right, rgba(0, 224, 255, 1), rgba(0, 133, 255, 1))` ,
+    justifyContent : "center" ,
+    justifyItems :"center" ,
+    borderRadius : "50px",
+    maxWidth : "100vh",
+    padding : "20px",
+  },
+
+ })) ;
 
 function App() {
+  const classes = useStyles();
+  const randomNum = Math.floor(Math.random() * Math.floor(Time.length))
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <div className={classes.root}>
+       <div className={classes.container}>
+            <h1>ALGO : {Time[randomNum].FIELD1}</h1>
+		       	<h2>WORSTCASE TIME COMPLEXITY:{Time[randomNum].FIELD2}</h2>
+             <Button variant="contained" onClick={refreshPage}>Click to revise!</Button>
+        </div>
+      </div>
+     </div>
   );
 }
 
---
+++ src/Time.js
@@ -0,0 +1,204 @@
+export const Time  = [
+    {
+        FIELD1 : "Array Access " ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Array Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Array Insertion " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Array Deletion" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Stack Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Stack Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Stack Insertion " ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Stack Deletion" ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Queue Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Queue Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Queue Insertion " ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Queue Deletion" ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Singly Linked list Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Singly Linked list Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Singly Linked list Insertion " ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Singly Linked list Deletion" ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Doubly Linked List Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Doubly Linked List Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Doubly Linked List Insertion " ,
+        FIELD2 : "O(1)" 
+    },
+    {
+        FIELD1 : "Doubly Linked List Deletion" ,
+        FIELD2 : "O(1)" 
+    },
+
+    {
+        FIELD1 : "Hash Table Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Hash Table Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Hash Table Insertion " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Hash Table Deletion" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary Search Tree Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary Search Tree Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary Search Tree Insertion " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary Search Tree Deletion" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "AVL Tree Access " ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "AVL Tree Search" ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "AVL Tree Insertion " ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "AVL Tree Deletion" ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "Binary  Tree Access " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary  Tree Search" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary Tree Insertion " ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Binary Tree Deletion" ,
+        FIELD2 : "O(N)" 
+    },
+    {
+        FIELD1 : "Red Black Tree Access " ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "Red Black Tree Search" ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "Red Black Tree Insertion " ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "Red Black Tree Deletion" ,
+        FIELD2 : "O(log N)" 
+    },
+    {
+        FIELD1 : "Selection Sort" ,
+        FIELD2 : "O(n^2)" 
+    },
+    {
+        FIELD1 : "Bubble Sort" ,
+        FIELD2 : "O(n^2)" 
+    },
+    {
+        FIELD1 : "Insertion Sort " ,
+        FIELD2 : "O(n^2)" 
+    },
+    {
+        FIELD1 : "Heap Sort " ,
+        FIELD2 : "O(n log(n))" 
+    },
+    {
+        FIELD1 : "Quick Sort" ,
+        FIELD2 : "θ(n log(n))" 
+    },
+    {
+        FIELD1 : "Merge Sort" ,
+        FIELD2 : "O(n log(n))" 
+    },
+    {
+        FIELD1 : "Bucket Sort " ,
+        FIELD2 : "O(n^2)" 
+    },
+
+    {
+        FIELD1 : "Radix Sort" ,
+        FIELD2 : "O(nk)" 
+    },
+    
+    {
+        FIELD1 : "Count Sort " ,
+        FIELD2 : "O(n+k)" 
+    }
+    
+
+    
+]
